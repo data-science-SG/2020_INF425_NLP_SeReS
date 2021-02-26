7c5dfe4894e8b68997706ca35661ca633c65ccd7
Bruno Fontana <fontanads@gmail.com>
2021-02-26 09:13:38 -0300

    reorganização de funcoes e pastas

---
+++ eda/funcs/__label_2_embedding__.py
@@ -0,0 +1,17 @@
+def label2Embedding(sentence):
+  """ Check if the word of the received sentence is included in the pre-trained word embedding and return it's embedding
+  Keywords arguments:
+  sentence -- tweet itself
+  """
+  print('HELLO CARLOS')
+  for word in sentence:
+    if word in embedding.vocab:
+      embed = embedding.get_vector(word)
+      return embed
+    else:
+      print("This word is not in the vocabuylary: ", word, "\n")
+
+# para testar a sua função
+if __name__=='__main__':
+  # código de teste das funções deste módulo
+  label2Embedding('test')
